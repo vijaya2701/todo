e369bc028f2b624a0f318b917baad8e6298a4bd8
vijaya2701 <vijaya2712001@gmail.com>
2021-02-05 16:06:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+export default class App extends Component{
+    constructor(){
+        super();
+        this.state={
+            formInputData:"",
+            storeData:[]
+        }
+    }
+    storeInputData=(event1)=>{
+        this.setState({
+            formInputData:event1.target.value
+        })
+    }
+    storeFinalData=()=>{
+        this.setState({
+            storeData:[...this.state.storeData,this.state.formInputData]
+        })
+    }
+    // deleting an item from array
+    deletestoreData=(event2)=>{
+        var temptData=this.state.storeData;
+         if(window.confirm("Are you sure ?")==true){
+             temptData.splice(Number(event2.target.id),1)
+             this.setState({
+                 storeData:temptData
+             })
+         }
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    render(){
+        return(
+            <div className="row">
+                {/* First column */}
+                <div className="col-lg-5 col-md-10">
+                  <div className="card mt-3">
+                      <div className="card-body">
+                          <form>
+                              <input type="text" className="form-control" placeholder="Add data..." onKeyUp={(inputEvent)=>{this.storeInputData(inputEvent)}}/>
+                              <br/>
+                              <button type="button" className="btn btn-primary" onClick={this.storeFinalData}>Add data </button>
+                          </form>
+                      </div>
+
+                  </div>
+                </div>
+                {/* second column */}
+                <div className="col-lg-5 col-md-10">
+                <div className="card mt-3">
+                      <div className="card-body">
+                      <ul className="list-group">{this.state.storeData.map((item,index)=>(
+                              <li className="list-group-item" key={index}>{item}<button className="btn btn-danger" id={index} onClick={(deleteEvent)=>this.deletestoreData(deleteEvent)}>delete</button></li>
+        ))}</ul>
+ 
+                      </div>
+
+                  </div>
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
